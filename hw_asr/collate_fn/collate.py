37dc6c031dc1d01c6b1ea2d6505507d96706b81b
s-isaev <48261488+s-isaev@users.noreply.github.com>
2021-10-09 17:42:19 +0300

    collate

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -1,5 +1,6 @@
 import logging
 from typing import List
+import torch
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +12,40 @@ def collate_fn(dataset_items: List[dict]):
 
     result_batch = {}
     # TODO: your code here
-    print(dataset_items)
 
-    raise NotImplementedError
+    max_spectogram_len = 0
+    max_text_encoded_len = 0
+    for dataset_item in dataset_items:
+        max_spectogram_len = max(
+            max_spectogram_len, dataset_item['spectrogram'].shape[2])
+        max_text_encoded_len = max(
+            max_text_encoded_len, dataset_item['text_encoded'].shape[1])
+
+    batch_spectograms = torch.zeros(
+        len(dataset_items),
+        dataset_items[0]['spectrogram'].shape[1],
+        max_spectogram_len
+    )
+    batch_text_encoded = torch.zeros(
+        len(dataset_items),
+        max_text_encoded_len
+    )
+    batch_text_encoded_length = torch.zeros(
+        len(dataset_items)
+    )
+    batch_text = []
+
+    for i, dataset_item in enumerate(dataset_items):
+        batch_spectograms[i, :, :dataset_item['spectrogram'].shape[2]] = \
+            dataset_item['spectrogram'].squeeze()
+        batch_text_encoded[i, :dataset_item['text_encoded'].shape[1]] = \
+            dataset_item['text_encoded'].squeeze()
+        batch_text_encoded_length[i] = dataset_item['text_encoded'].shape[1]
+        batch_text.append(dataset_item['text'])
+
+    result_batch['spectrogram'] = batch_spectograms
+    result_batch['text_encoded'] = batch_text_encoded
+    result_batch['text_encoded_length'] = batch_text_encoded_length
+    result_batch['text'] = batch_text
+
+    return result_batch
