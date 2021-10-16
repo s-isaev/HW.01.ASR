72681be8730c251e9ccf9cf0fe368b46eabcb378
s-isaev <48261488+s-isaev@users.noreply.github.com>
2021-10-16 19:50:18 +0300

    Update __init__.py

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,5 +1,6 @@
 from hw_asr.model.baseline_model import BaselineModel
 from hw_asr.model.overfit_model import OverfitModel
+from hw_asr.model.model_01 import Model01
 
 __all__ = [
     "BaselineModel", "OverfitModel", "Model01"
