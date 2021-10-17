26bdeeb790f60e4cec795b4aee80bb5966db51eb
s-isaev <48261488+s-isaev@users.noreply.github.com>
2021-10-17 17:45:40 +0300

    model 02

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,7 +1,8 @@
 from hw_asr.model.baseline_model import BaselineModel
 from hw_asr.model.overfit_model import OverfitModel
 from hw_asr.model.model_01 import Model01
+from hw_asr.model.model_02 import Model02
 
 __all__ = [
-    "BaselineModel", "OverfitModel", "Model01"
+    "BaselineModel", "OverfitModel", "Model01", "Model02"
 ]
