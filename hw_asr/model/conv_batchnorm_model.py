93e68cfe8baae040d3236c6563654ce7cf6e9c4f
s-isaev <48261488+s-isaev@users.noreply.github.com>
2021-10-19 01:28:29 +0300

    Update conv_batchnorm_model.py

--- hw_asr/model/conv_batchnorm_model.py
+++ hw_asr/model/conv_batchnorm_model.py
@@ -14,7 +14,6 @@ class ConvBatchnormModel(BaseModel):
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=2, kernel_size=4, padding='same')
         self.conv2 = nn.Conv1d(in_channels=2, out_channels=2, kernel_size=4, padding='same')
         self.conv3 = nn.Conv1d(in_channels=2, out_channels=2, kernel_size=4, padding='same')
-        self.bn2 = nn.BatchNorm1d(256)
 
         self.lstm1 = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
         self.lstm2 = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
@@ -35,8 +34,6 @@ class ConvBatchnormModel(BaseModel):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x = x.reshape(batch_size*spect_size, 256)
-        x = self.bn2(x)
         x = x.reshape(batch_size, spect_size, 256)
 
         
