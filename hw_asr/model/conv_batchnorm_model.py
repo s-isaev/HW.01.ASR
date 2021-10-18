3af84f8f4d16dbf5db585d7c15839ab1effcd2d5
s-isaev <48261488+s-isaev@users.noreply.github.com>
2021-10-19 01:38:02 +0300

    Update conv_batchnorm_model.py

--- hw_asr/model/conv_batchnorm_model.py
+++ hw_asr/model/conv_batchnorm_model.py
@@ -10,40 +10,30 @@ class ConvBatchnormModel(BaseModel):
     def __init__(self, n_feats, n_class, *args, **kwargs):
         super().__init__(n_feats, n_class, *args, **kwargs)
 
-        self.bn1 = nn.BatchNorm1d(128)
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=2, kernel_size=4, padding='same')
-        self.conv2 = nn.Conv1d(in_channels=2, out_channels=2, kernel_size=4, padding='same')
-        self.conv3 = nn.Conv1d(in_channels=2, out_channels=2, kernel_size=4, padding='same')
-
-        self.lstm1 = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
-        self.lstm2 = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
-        self.lstm3 = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
-        self.lstm4 = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
-        self.lstm5 = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
-        self.fc = nn.Linear(in_features=256, out_features=n_class)
+        self.lstm1  = nn.LSTM(input_size=n_feats, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
+        self.relu1 = nn.ReLU()
+        self.lstm2  = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
+        self.relu2 = nn.ReLU()
+        self.lstm3  = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
+        self.relu3 = nn.ReLU()
+        self.lstm4  = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
+        self.relu4 = nn.ReLU()
+        self.lstm5  = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
+        self.relu5 = nn.ReLU()
+        self.linear = nn.Linear(in_features=256, out_features=n_class)
 
     def forward(self, spectrogram, *args, **kwargs):
-        x = spectrogram
-
-        batch_size = x.shape[0]
-        spect_size = x.shape[1]
-
-        x = x.reshape(batch_size*spect_size, 128)
-        x = self.bn1(x)
-        x = x.reshape(batch_size*spect_size, 1, 128)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv3(x))
-        x = x.reshape(batch_size, spect_size, 256)
-
-        
-        x = F.relu(self.lstm1(x)[0])
-        x = F.relu(self.lstm2(x)[0])
-        #x = F.relu(self.lstm3(x)[0])
-        #x = F.relu(self.lstm4(x)[0])
-        #x = F.relu(self.lstm5(x)[0])
-        x = self.fc(x)
-
-        return x
+        x, (hn, cn) = self.lstm1(spectrogram)
+        x = self.relu1(x)
+        x, (hn, cn) = self.lstm2(x)
+        x = self.relu2(x)
+        x, (hn, cn) = self.lstm3(x)
+        x = self.relu3(x)
+        x, (hn, cn) = self.lstm4(x)
+        x = self.relu4(x)
+        x, (hn, cn) = self.lstm5(x)
+        x = self.relu5(x)
+        return self.linear(x)
 
     def transform_input_lengths(self, input_lengths):
         return input_lengths  # we don't reduce time dimension here
